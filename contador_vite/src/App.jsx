63a5156d634cc763a751ac727d2e0e957e74955b
Jesus-Barraza <190627332+Jesus-Barraza@users.noreply.github.com>
2026-02-05 12:39:26 -0600

    Update App.jsx

--- contador_vite/src/App.jsx
+++ contador_vite/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 import Inicio from "../components/Inicio"
+import Tareas from "../components/Tareas"
+
+//<Inicio/>
 
 function App() {
   return (
     <>
-    <Inicio/>
+    <Tareas/>
     </>
   )
 }
